747a8180880612fc0fe05537baa752367008f4d8
marsjava <marikannan.cvp@gmail.com>
2018-03-24 10:03:54 +0530

    overloading commit

--- corejava/src/main/java/com/eyeopen/modifier/A.java
+++ corejava/src/main/java/com/eyeopen/modifier/A.java
@@ -3,18 +3,22 @@ class A{
  void display(){
   System.out.println("Hi, this is display() default");
  }
-// void display(String city){	//compilation error 
-// }
  void display(String name){
   System.out.println("Hi, "+name);
  }
  void display(int a, int b){
    System.out.println("a value is "+a+" b value is "+b);
  }
+ double display(double radius) {
+	 double aoc = (22/7)*radius*radius;
+	 return aoc;
+ }
  public static void main(String[] args){
    A obj = new A();
    obj.display(10, 12); 
    obj.display();
-   obj.display("Santhan");   
+   obj.display("Santhan");
+   double res = obj.display(6.5);
+   System.out.println("Area of circle is "+res);
  } 
 }
